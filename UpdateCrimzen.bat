23b1fa04208f489eda65bbc5958245667adf2c28
XavierHale <81330980+XavierHale@users.noreply.github.com>
2021-07-08 15:30:37 -0400

    Updater Changed
    Added recognition for a common bug and moved all application files to a separate folder

--- UpdateCrimzen.bat
+++ UpdateCrimzen.bat
@@ -2,7 +2,17 @@
 :top
 ping 0.0.0.0 >nul
 git pull https://github.com/XavierHale/Crimzen-Client
+if %ERRORLEVEL%=0 (
 cd \Crimzen-Client
 type Ver
+) else (
+    echo Update unsuccesful
+    if %ERRORLEVEL%=128 (
+        echo Make sure this file is in the same directory as your CRIMZEN folder
+    ) else (
+        echo Unknown error. Code=%ERRORLEVEL%
+    )
+)
+
 pause
 goto :top
