7f02ba330653bfdfe52566b14b34b24f2191365c
XavierHale <81330980+XavierHale@users.noreply.github.com>
2021-10-22 15:47:33 -0400

    undated things

--- Launcher.bat
+++ Launcher.bat
@@ -1,15 +1,18 @@
 @echo off
-mode con: cols=32 lines=20
+mode con: cols=32 lines=15
 title Crimzen Bootmanager 1.0
-echo      ^|---------------------^|
-echo      ^| Crimzen Bootmanager ^|
-echo      ^|---------------------^|
+echo ^|---------------------^|
+echo ^| Crimzen Bootmanager ^|
+echo ^|---------------------^|
 echo This window will display messages sent from the Crimzen 
-pause
 cd CRIMZEN 
 FOR /F "tokens=* delims=" %%x in (ver.crm) DO echo Loading Version %%x
 start CRIMZENV3.1.2.bat 
+timeout 5 >nul
 :listerror
+cls
+echo Pulling error data...
+ping 0.0.0.0 >nul
 echo %ERRORLEVEL%
 type errmsg.crm
 break>errmsg.crm
